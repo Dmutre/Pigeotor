5fe865537f3618fda4aaa5592cdb168f054b8df0
Dmutre <dmytro.lesko@gmail.com>
2023-07-10 20:51:46 +0200

    0.0.2
    fixed problem with view engine

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 "use strict";
+//Import our modules
 var express = require("express");
 var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 3000;
 var app = express();
-app.engine("handlebars", exphbs({
-    layoutsDir: __dirname + '/src/views/layout',
-}));
-app.set("view engine", "handlebars");
+//Routers
+var authRouter = require("./src/router/authRouter.js");
+//Set view engine. We set handlebars
+var handlebars = exphbs.create({ extname: '.hbs', defaultLayout: "layout" });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+app.set("views", "./src/views");
+app.set("layout", "layout");
+app.use("/", authRouter);
 app.listen(PORT, function () {
     console.log("We listen port: " + PORT);
 });
--- server.ts
+++ server.ts
@@ -1,15 +1,24 @@
 "use strict";
 
+//Import our modules
 const express = require("express");
 const exphbs  = require("express-handlebars");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.engine("handlebars", exphbs({
-  layoutsDir: __dirname + '/src/views/layout',
-}));
-app.set("view engine", "handlebars");
+//Routers
+const authRouter = require("./src/router/authRouter.js");
+
+//Set view engine. We set handlebars
+const handlebars = exphbs.create({ extname: '.hbs', defaultLayout: "layout" });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+app.set("views", "./src/views");
+app.set("layout", "layout");
+
+
+app.use("/", authRouter);
 
 app.listen(PORT, () => {
   console.log("We listen port: " + PORT);
